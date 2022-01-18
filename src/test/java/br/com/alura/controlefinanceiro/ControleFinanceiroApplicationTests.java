759aa0c287ffb941fe08a6b4e7063d7125c93247
Wesley <wesleymdeandrade@gmail.com>
2022-01-18 07:30:27 -0300

    Adiciona arquivos padrão do Spring Boot e Maven

---
+++ src/test/java/br/com/alura/controlefinanceiro/ControleFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.controlefinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
