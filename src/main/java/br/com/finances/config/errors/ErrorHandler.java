d3420ad12111f11e03b5ec013d8ae2b709043948
Wesley <wesleymdeandrade@gmail.com>
2022-01-29 02:30:12 -0300

    Add error message when a post or update have the same description and month as an already registered.

--- src/main/java/br/com/finances/config/errors/ErrorHandler.java
+++ src/main/java/br/com/finances/config/errors/ErrorHandler.java
@@ -3,6 +3,8 @@ package br.com.finances.config.errors;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityExistsException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -15,14 +17,20 @@ public class ErrorHandler {
 
 	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public List<ErrorDto> handle(MethodArgumentNotValidException exception) {
-		List<ErrorDto> errorDto = new ArrayList<>();
+	public List<ErrorFormDTO> handle(MethodArgumentNotValidException exception) {
+		List<ErrorFormDTO> errorFormDto = new ArrayList<>();
 		
 		List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
 		fieldErrors.forEach(e -> {
-			errorDto.add(new ErrorDto(e.getField(), e.getDefaultMessage()));
+			errorFormDto.add(new ErrorFormDTO(e.getField(), e.getDefaultMessage()));
 		});
 		
-		return errorDto;		
+		return errorFormDto;		
+	}
+	
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(EntityExistsException.class)
+	public ErrorDTO handle(EntityExistsException exception) {
+		return new ErrorDTO("There is already an entity with this description in this month registered.");
 	}
 }
