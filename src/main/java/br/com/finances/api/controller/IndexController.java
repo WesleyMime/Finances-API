8fb3d0cae615f88822cb0fe67f42b341cd24d885
Wesley M <wesleymdeandrade@gmail.com>
2022-01-30 07:22:53 -0300

    Add controller to customize error page when a path doesn't exists.

---
+++ src/main/java/br/com/finances/api/controller/IndexController.java
@@ -0,0 +1,18 @@
+package br.com.finances.api.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController implements ErrorController {
+	
+    @RequestMapping("/error")
+    public String error() {
+        return "<h1>Finances API</h1>"
+        		+ "<h2><a "
+        		+ "href=\"https://documenter.getpostman.com/view/19203694/UVeCPnzs\" "
+        		+ "target=\"_blank\">Documentation"
+        		+ "</a></h2>";
+    }
+}
