b86340a01fc8a124ccfafc52b6b3a90822e1b26c
Wesley M <wesleymdeandrade@gmail.com>
2022-01-30 05:55:03 -0300

    Add HttpSecurity configuration to require https connection.

--- src/main/java/br/com/finances/api/service/SummaryService.java
+++ src/main/java/br/com/finances/api/service/SummaryService.java
@@ -2,7 +2,6 @@ package br.com.finances.api.service;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
--- src/main/java/br/com/finances/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/finances/config/security/SecurityConfigurations.java
@@ -9,18 +9,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic().disable().csrf().disable().authorizeRequests().anyRequest().permitAll();
+	protected void configure(HttpSecurity http) throws Exception {		
+		http.requiresChannel()	// Force the use of HTTPS
+			.requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+			.requiresSecure()
+		.and()
+		.httpBasic().disable()
+		.csrf().disable()
+		.authorizeRequests().anyRequest().permitAll();
 	}
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// TODO Auto-generated method stub
 		super.configure(auth);
 	}
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		// TODO Auto-generated method stub
