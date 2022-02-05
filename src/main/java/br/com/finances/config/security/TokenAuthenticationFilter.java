7a228486fb34eb79b9780bb2febe6d0d6ef10f67
Wesley M <wesleymdeandrade@gmail.com>
2022-02-04 23:26:05 -0300

    Stateless Authentication using JWT.

---
+++ src/main/java/br/com/finances/config/security/TokenAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package br.com.finances.config.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.finances.api.service.TokenService;
+import br.com.finances.model.Client;
+import br.com.finances.repository.ClientRepository;
+
+public class TokenAuthenticationFilter extends OncePerRequestFilter{
+
+	private TokenService tokenService;
+	private ClientRepository clientRepository;
+	
+	public TokenAuthenticationFilter(TokenService tokenService, ClientRepository clientRepository) {
+		this.tokenService = tokenService;
+		this.clientRepository = clientRepository;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		String token = getToken(request);
+		
+		boolean valid = tokenService.isValid(token);
+		if(valid) {
+			authenticate(token);
+		}
+		
+		
+		filterChain.doFilter(request, response);
+		
+	}
+
+	private String getToken(HttpServletRequest request) {
+		String token = request.getHeader("Authorization");
+		if(token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+			return null;
+		}
+		return token.substring(7);
+	}
+	
+	private void authenticate(String token) {
+		Long idClient = tokenService.getIdClient(token);
+		Client client = clientRepository.findById(idClient).get();
+		
+		UsernamePasswordAuthenticationToken authentication = 
+				new UsernamePasswordAuthenticationToken(client, null, client.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+	}
+}
