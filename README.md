f462806a5894336379b3758d128b2fba5ab9b1bf
Wesley M <wesleymdeandrade@gmail.com>
2022-02-07 01:17:49 -0300

    Update documentation link

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ API REST feita para o Alura Challenge Backend 2
 https://finance-api-challenge.herokuapp.com/
 
 <h3>Documentação da API:</h3>
-https://documenter.getpostman.com/view/19203694/UVeGr6Z9
+https://documenter.getpostman.com/view/19203694/UVeGs6cv
--- src/main/java/br/com/finances/api/controller/IndexController.java
+++ src/main/java/br/com/finances/api/controller/IndexController.java
@@ -11,7 +11,7 @@ public class IndexController implements ErrorController {
     public String error() {
         return "<h1>Finances API</h1>"
         		+ "<h2><a "
-        		+ "href=\"https://documenter.getpostman.com/view/19203694/UVeCPnzs\" "
+        		+ "href=\"https://documenter.getpostman.com/view/19203694/UVeGs6cv\" "
         		+ "target=\"_blank\">Documentation"
         		+ "</a></h2>";
     }
