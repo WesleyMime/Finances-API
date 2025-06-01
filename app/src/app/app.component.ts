34cfea3a15241e4d9733a2ac7366bc02ca95a058
Wesley M. <55067868+WesleyMime@users.noreply.github.com>
2025-06-01 04:41:09 -0300

    feat: app routes

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, FooterComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'app';
--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AddTransactionComponent } from './add-transaction/add-transaction.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { authGuard } from './auth/auth.guard';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ReportsComponent } from './reports/reports.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: HomeComponent},
+    {path: "register", component: RegisterComponent},
+    {path: "login", component: LoginComponent},
+    {path: "add-transaction", component: AddTransactionComponent, canActivate: [authGuard]},
+    {path: "dashboard", component: DashboardComponent, canActivate: [authGuard]},
+    {path: "reports", component: ReportsComponent, canActivate: [authGuard]},
+];
