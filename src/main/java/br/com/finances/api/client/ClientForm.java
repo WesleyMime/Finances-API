125daa4d0a0c83d60a67915754e3fc6fd61ddf42
Wesley M. <55067868+WesleyMime@users.noreply.github.com>
2025-06-24 05:45:25 -0300

    feat: enhance client model with validation and update patch functionality

--- src/main/java/br/com/finances/api/client/ClientForm.java
+++ src/main/java/br/com/finances/api/client/ClientForm.java
@@ -1,4 +1,9 @@
 package br.com.finances.api.client;
 
-public record ClientForm(String name, String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
+public record ClientForm(@Size(min = 3, max = 255) String name,
+						 @Size(min = 5, max = 255) @Email String email,
+						 @Size(min = 8, max = 255) String password) {
 }
