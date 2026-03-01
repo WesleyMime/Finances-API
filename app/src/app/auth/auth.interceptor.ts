86bd6521b708f3aebb0080fea4f09cdf4746336c
Wesley M. <55067868+wesleymime@users.noreply.github.com>
2026-03-01 18:04:33 -0300

    fix: isLoading status not working when logging in with demo account from home page

--- app/src/app/auth/auth.interceptor.ts
+++ app/src/app/auth/auth.interceptor.ts
@@ -1,19 +1,13 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from './auth.service';
-import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   let authService = inject(AuthService);
-  let router = inject(Router);
   let authCookie = authService.getAuthCookie();
-  if (!authService.isLoggedIn() && !router.url.includes("register")) {
-    router.navigate(['/login']);
-  }
-
+  
   const newReq = req.clone({
     headers: req.headers.append('Authorization', "Bearer " + authCookie)
   });
-
   return next(newReq);
 };
